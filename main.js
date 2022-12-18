eada387bdbe7bbe36333ee785486908ee6a7225b
ProgramadorProfesional <116525245+ProgramadorProfesional@users.noreply.github.com>
2022-12-18 20:05:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function function1() {
+    document.getElementById("audio1").play();
+}
+
+
+
+
+
+
+function function2() {
+    document.getElementById("audio2").play();
+}
+
+
+
+function function3() {
+    document.getElementById("audio3").play();
+}
+
